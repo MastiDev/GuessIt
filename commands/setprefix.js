3c71affce127a74d8a2b6b6bd93e418001d32a28
Masti <65887954+MastiderMast@users.noreply.github.com>
2021-10-18 15:04:49 +0200

    Update setprefix

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -7,15 +7,10 @@ module.exports = new Command({
 	aliases: [],
 	
 	async run(message, args, con, serverstats, userstats, client) {
-		
 		try {
 		if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("You don't have enough permissions!");
 		if(!(args.length > 1)) return message.reply("not enough arguments!")
-		const newprefix = args[1];
-			sql = `UPDATE serverstats SET prefix = '${newprefix}' WHERE serverid = '${message.guild.id}'`
-			con.query(sql, (err, rows) => {
-				if(err) throw err;
-			})
+			con.query(`UPDATE serverstats SET prefix = '${args[1]}' WHERE serverid = '${message.guild.id}'`)
 			message.channel.send("The Prefix is now " + newprefix)
 		} catch (error) {
 			console.log(red(`[ERROR] In the command ${this.name} an error has occurred -> ${error}`))
