b0fb11b60fe761a7a6c68cede2e742c06997246a
MastiDev <65887954+MastiDev@users.noreply.github.com>
2023-09-01 17:50:30 +0200

    Added delete cmd

---
+++ src/interactions/slashCommands/delete.ts
@@ -0,0 +1,28 @@
+import { Client, ChatInputCommandInteraction, TextBasedChannel, Message } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder } from '@discordjs/builders';
+
+const commandID = 'delete';
+export default {
+	id: commandID,
+	type: 'slashCommand',
+	disabled: false,
+	data: new SlashCommandBuilder()
+		.setName(commandID)
+		.addChannelOption(option => option.setName('channel').setDescription('Channel').setRequired(true))
+		.setDescription('delete'),
+	async execute(client: Client, interaction: ChatInputCommandInteraction) {
+		try {
+			const channel = interaction.options.getChannel('channel');
+			if (!channel) return interaction.reply('You have to mention a channel!');
+
+			const round = client.Eround.get(channel.id);
+			if (!round) return  interaction.reply('In this Channel is no Round');
+
+			client.Eround.delete(channel.id);
+
+			await interaction.reply('The round has been deleted!');
+		} catch (error) {
+			console.log(error);
+		}
+	},
+};
